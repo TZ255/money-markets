9bbb7288b1332fa59f4734823c1ef902e4fefff3
Vatsal6288 <kachhadiyavatsal1@gmail.com>
2026-02-16 13:05:44 +0530

    refactor(update): updated integrate TooltipProvider in layout & update Radix imports

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,7 +1,4 @@
-'use client'
-
 import * as React from 'react'
-
 import { Tooltip as TooltipPrimitive } from 'radix-ui'
 
 import { cn } from '@/lib/utils'
@@ -11,11 +8,7 @@ function TooltipProvider({ delayDuration = 0, ...props }: React.ComponentProps<t
 }
 
 function Tooltip({ ...props }: React.ComponentProps<typeof TooltipPrimitive.Root>) {
-  return (
-    <TooltipProvider>
-      <TooltipPrimitive.Root data-slot='tooltip' {...props} />
-    </TooltipProvider>
-  )
+  return <TooltipPrimitive.Root data-slot='tooltip' {...props} />
 }
 
 function TooltipTrigger({ ...props }: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
